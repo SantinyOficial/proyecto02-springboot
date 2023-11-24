3c2414bc3dc60bbe4c2082888388f2e11ee5c1ca
SantinyOficial <santiunido@gmail.com>
2023-11-23 20:43:13 -0500

    Reportes en Excel y PDF

--- src/main/java/com/tcna/gestioncursos/controller/CursoController.java
+++ src/main/java/com/tcna/gestioncursos/controller/CursoController.java
@@ -1,7 +1,10 @@
 package com.tcna.gestioncursos.controller;
 
 import com.tcna.gestioncursos.entity.Curso;
+import com.tcna.gestioncursos.reports.CursoExporterPDF;
+import com.tcna.gestioncursos.reports.CursosExporterExcel;
 import com.tcna.gestioncursos.repository.CursoRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.awt.*;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -48,5 +56,35 @@ public class CursoController {
             redirectAttributes.addAttribute("message", e.getMessage());
         }
         return"redirect:/cursos";
+    }
+
+    @GetMapping("/export/pdf")
+    public void generarReportePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=cursos" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<Curso> cursos = cursoRepository.findAll();
+        CursoExporterPDF exporterPDF = new CursoExporterPDF(cursos);
+        exporterPDF.export(response);
+    }
+
+    @GetMapping("/export/excel")
+    public void generarReporteExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=cursos" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<Curso> cursos = cursoRepository.findAll();
+        CursosExporterExcel exporterExcel = new CursosExporterExcel(cursos);
+        exporterExcel.export(response);
     }
 }
